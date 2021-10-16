0b832d6edd1b284acaf6432738fe7a2b0a9bc3db
Eko Prasetyo <prasetyodesign@gmail.com>
2021-10-16 18:50:32 +0700

    test route again

--- routes/routes.tele.js
+++ routes/routes.tele.js
@@ -4,30 +4,9 @@ var api = require('../core/core.tele')
 
 // Create routes
 
-Object.keys(api).forEach(baseapi => {
-    var routes = api[baseapi];
-    
-    var routes = api[baseapi];
-
-    for (var [routeinfo, command] of Object.entries(routes)) {
-
-        var [method, route] = routeinfo.split('|')
-
-        //route = baseapi + route
-        
-        router[method](route, async function(req, res, next) {
-            if(req.is('text/*')) {
-                req.setEncoding('utf8');
-                req.rawBody = '';
-                req.on('data', function(chunk) {
-                    req.rawBody += chunk;
-                });
-                req.on('end', function(){
-                    next();
-                });
-                res.send(req.rawBody);
-            }
-        })
-    }
+var app = express();
+app.get('/v1', function (req, res) {
+    res.send('hello world');
 });
+
 module.exports = router;
