57713a2c1cc7d2e14d9efc51c4113a7366da7935
Eko Prasetyo <prasetyodesign@gmail.com>
2021-10-16 19:19:08 +0700

    parse raw body

--- routes/routes.tele.js
+++ routes/routes.tele.js
@@ -1,12 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+function rawBody(req, res, next) {
+    req.setEncoding('utf8');
+    req.rawBody = '';
+    req.on('data', function(chunk) {
+        req.rawBody += chunk;
+    });
+    req.on('end', function(){
+        next();
+    });
+}
+
+router.use(rawBody());
+
 // Create routes
 router.get('/', function(req, res, next) {
     res.send('hello world');
 });
 
 router.post('/', function(req, res, next) {
-    res.send('hello world');
+    // res.send('hello world');
+    res.send(req.rawBody);
 });
 module.exports = router;
